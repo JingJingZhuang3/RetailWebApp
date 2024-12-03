aa7ced4b80822b110562325b89f421f7056a46ce
Jingjing Zhuang <jzhuang3219@gmail.com>
2024-12-03 17:13:06 -0500

    Added bands & types specification

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -29,9 +29,10 @@ public class BaseSpecification<T>(Expression<Func<T, bool>>? criteria) : ISpecif
     }
 }
 
-public class BaseSpecification<T, TResult>(Expression<Func<T, bool>> criteria)
+public class BaseSpecification<T, TResult>(Expression<Func<T, bool>>? criteria)
 : BaseSpecification<T>(criteria), ISpecification<T, TResult>
 {
+    protected BaseSpecification() : this(null) { }
     public Expression<Func<T, TResult>>? Select { get; private set; }
     protected void AddSelect(Expression<Func<T, TResult>> selectExpression)
     {
